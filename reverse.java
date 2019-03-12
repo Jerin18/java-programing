3b714ca9b8eecc23d049b2e8d47f72379fa3f76c
jerin189 <jeringsaji@gmail.com>
2019-03-12 14:49:33 +0530

    reverse

--- reverse.java
+++ reverse.java
@@ -1,15 +1,15 @@
 
 import java.util.Scanner;
 public class reverse{
-public static void main(string[] args){
- Scanner myinput=new Scanner("System.int");
+public static void main(String[] args) {
+ Scanner myinput=new Scanner(System.in);
  int n,s,r=0;
  System.out.println("enter the number");
  n=myinput.nextInt();
  while(n!=0)
  {
   s=n%10;
-  r=r*10+r;
+  r=r*10+s;
   n=n/10;
  }
  System.out.println("reverse="+r);
